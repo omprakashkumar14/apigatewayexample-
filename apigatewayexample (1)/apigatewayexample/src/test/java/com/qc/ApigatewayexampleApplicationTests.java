d46a8d49b80ab846bcf317d811fe9e526d4338e2
omprakashkumar14 <155672096+omprakashkumar14@users.noreply.github.com>
2025-02-05 21:03:13 +0530

    apigatewayexample
    apigatewayexample

---
+++ apigatewayexample (1)/apigatewayexample/src/test/java/com/qc/ApigatewayexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
